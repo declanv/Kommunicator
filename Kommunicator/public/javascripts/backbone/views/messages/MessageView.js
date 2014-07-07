80a08a48854105a02db56e913ad272440777edc2
Crustacean87! <declanv@gmail.com>
2014-07-06 21:30:27 -0400

    Started on backbone view for messages, and added in template in the index.html file to append into the layout

---
+++ Kommunicator/public/javascripts/backbone/views/messages/MessageView.js
@@ -0,0 +1,9 @@
+var Kommunicatorapp = Kommunicatorapp || { Models: {}, Collections: {}, Views: {} };
+
+
+Kommunicatorapp.Views.Message = Backbone.Model.extend({
+  initialize: function(){
+    console.log("You've initialized a view")
+    }
+  }
+})
