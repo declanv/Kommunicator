30a56daaf33cb55ef8210c2ecb7886b26e2ed074
Crustacean87! <declanv@gmail.com>
2014-07-06 23:13:50 -0400

    Added in an app initialize method, and a jqeury event listener for new messages therein

--- Kommunicator/public/javascripts/backbone/app.js
+++ Kommunicator/public/javascripts/backbone/app.js
@@ -1,10 +1,23 @@
 var Kommunicatorapp = Kommunicatorapp || { Models: {}, Collections: {}, Views: {} };
 
+Kommunicatorapp.initialize = function(){
 
+  var listView = new Kommunicatorapp.Views.MessageListView({
+    collection: collection,
+    el: $(".message_list")
+  });
 
+  $("#new_message_form").on('submit', function(e){
+    e.preventDefault();
+    var messageName = $('input').val();
+    $('input').val('');
+    collection.create({title: messageName});
+  })
 
-
+}
 
 $(function(){
+
+  Kommunicatorapp.initialize();
 
 });
