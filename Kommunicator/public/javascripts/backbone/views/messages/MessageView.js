91d1b117700ba6c25daa5a56979f10fe82122cc5
Crustacean87! <declanv@gmail.com>
2014-07-06 21:36:15 -0400

    Added in the render funtion for the message template

--- Kommunicator/public/javascripts/backbone/views/messages/MessageView.js
+++ Kommunicator/public/javascripts/backbone/views/messages/MessageView.js
@@ -4,6 +4,15 @@ var Kommunicatorapp = Kommunicatorapp || { Models: {}, Collections: {}, Views: {
 Kommunicatorapp.Views.Message = Backbone.Model.extend({
   initialize: function(){
     console.log("You've initialized a view")
+    this.listenTo( this.model, "change", this.render);
+    },
+    tagName: 'li',
+    template: _.template( $("#message_template").html() )
+    render: function(){
+      this.$el.empty();
+      this.$el.html( (this.template.model.attributes ) );
+      return this
     }
-  }
+
+
 })
