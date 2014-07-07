3d1181369c210340c58865eee1a8d897d09b18e8
Crustacean87! <declanv@gmail.com>
2014-07-06 20:55:18 -0400

    Added in create and index methods for both messages and comments

--- Kommunicator/app/controllers/comments_controller.rb
+++ Kommunicator/app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+respond_to :json
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :id, :message_id)
+  end
+
 end
--- Kommunicator/app/controllers/messages_controller.rb
+++ Kommunicator/app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+
+respond_to :json
+
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :id)
+  end
+
 end
